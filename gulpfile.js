907ed4702287aa418dfc6740995f5a4546672e07
Arkadiusz <nedar3@gmail.com>
2017-11-22 16:40:52 +0100

    add typescript support for gulp, add tests to check if everything is working at project start

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,72 @@
-var gulp         = require("gulp"),
-    watch        = require("gulp-watch"),
-    sass         = require("gulp-sass"),
+var gulp = require("gulp"),
+    ts = require("gulp-typescript"),
+    watch = require("gulp-watch"),
+    sass = require("gulp-sass"),
     autoprefixer = require("gulp-autoprefixer"),
-    browserSync  = require("browser-sync"),
-    nodemon      = require("gulp-nodemon");
+    browserSync = require("browser-sync"),
+    nodemon = require("gulp-nodemon");
 
 
+
+var tsProject = ts.createProject("tsconfig.json");
 
 var input = "./public/scss/*.scss";
 var output = "./public/css"
 
 var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'expanded'
+    errLogToConsole: true,
+    outputStyle: 'expanded'
 };
 
 
 
-gulp.task("sass", function (){
-   return gulp.src(input)
-    .pipe(sass(sassOptions).on("error", sass.logError))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest(output))
+gulp.task("sass", function() {
+    return gulp.src(input)
+        .pipe(sass(sassOptions).on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(output))
 });
 
-gulp.task("default", ["watch"], function (){  
+gulp.task("typescript", function() {
+    return tsProject.src()
+        .pipe(tsProject())
+        .pipe(gulp.dest("./public/js"));
 });
 
+gulp.task("default", ["watch"], function() {});
 
-gulp.task("watch", ["browser-sync"], function(){
-   
-   watch ("./public/scss/**/*scss", function (){
-      gulp.start("sass");
-    
-    
-   });
+
+gulp.task("watch", ["browser-sync"], function() {
+
+    watch("./public/scss/**/*.scss", function() {
+        gulp.start("sass");
+    });
+
+    watch("./public/ts/*.ts", function() {
+        gulp.start("typescript");
+    });
+
 });
 
-gulp.task("browser-sync", ["nodemon"], function(){
-  browserSync.init(null, {
-    proxy: "http://localhost:5000",
-    files: ["public/**/*.*", "views/*.*"],
-    port: 7000,
-    reloadDelay: 150,
-  });
+gulp.task("browser-sync", ["nodemon"], function() {
+    browserSync.init(null, {
+        proxy: "http://localhost:5000",
+        files: ["public/**/*.*", "views/*.*"],
+        port: 7000,
+        reloadDelay: 150,
+    });
 });
 
-gulp.task("nodemon", function (cb){
-  
-  var started = false;
-  
-  return nodemon({
-    script: "app.js"
-  }).on("start", function(){
-    if (!started) {
-      cb();
-      started = true;
-    }
-  });  
+gulp.task("nodemon", function(cb) {
+
+    var started = false;
+
+    return nodemon({
+        script: "app.js"
+    }).on("start", function() {
+        if (!started) {
+            cb();
+            started = true;
+        }
+    });
 });
-
-
